8da92c96a3f7fb4c9808ef1e4e6fc4aa287a59ae
Redflame39 <vanya133719@gmail.com>
2022-02-10 18:38:26 +0300

    Initial commit

---
+++ src/main/java/com/epam/esm/RestApiBootApplication.java
@@ -0,0 +1,13 @@
+package com.epam.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiBootApplication.class, args);
+    }
+
+}
